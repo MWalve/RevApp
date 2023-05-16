8e3f1e729b69baf75b051495cfd04d053e005311
MihirW <mihirw@hotmail.com>
2023-05-16 16:53:48 +0530

    made-welcome-page-setup

--- app/index.js
+++ app/index.js
@@ -1,21 +1,51 @@
-import { View, ScrollViewProps, SafeAreaView } from 'react-native';
-import { useState } from 'react';
-import { Stack, useRouter } from 'expo-router';
-
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import { COLORS, icons, images, SIZES } from '../constants';
-import { Welcome } from '../components';
 
 const Home = () => {
-    return (
-        <SafeAreaView style={{ flex: 1,
-        backgroundColor: COLORS.revblue }}>
-            <Stack.Screen
-            options={{
-                headerStyle: { backgroundColor: COLORS.lightWhite }, 
-                }} 
-                />
-        </SafeAreaView>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Image source={icons.whitehomecircle} 
+      style={styles.backgroundImage} resizeMode="cover" />
+      <View style={styles.contentContainer}>
+        <Image source={icons.RevLogo} 
+        style={styles.logoImage} resizeMode="contain" />
+        <Text style={styles.title}>RevApp</Text>
+        <Text style={styles.tagline}>Trust Your Gut!</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.revblue,
+  },
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: -1,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoImage: {
+    width: 150,
+    height: 150,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10,
+  },
+  tagline: {
+    fontSize: 16,
+    color: 'black',
+    marginBottom: 300,
+  },
+});
 
 export default Home;
