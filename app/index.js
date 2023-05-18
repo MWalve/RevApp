570e0623126ece89da1711540d2fe85055512756
MihirW <mihirw@hotmail.com>
2023-05-18 12:10:19 +0530

    changed background

--- app/index.js
+++ app/index.js
@@ -5,11 +5,13 @@ import { COLORS, icons, images, SIZES } from '../constants';
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Image source={icons.whitehomecircle} 
-      style={styles.backgroundImage} resizeMode="cover" />
+      <View style={styles.backgroundContainer}>
+        <Image source={icons.whitehomecircle} style={styles.backgroundImage} resizeMode="cover" />
+      </View>
       <View style={styles.contentContainer}>
-        <Image source={icons.RevLogo} 
-        style={styles.logoImage} resizeMode="contain" />
+        <View style={styles.logoContainer}>
+          <Image source={icons.RevLogo} style={styles.logoImage} resizeMode="contain" />
+        </View>
         <Text style={styles.title}>RevApp</Text>
         <Text style={styles.tagline}>Trust Your Gut!</Text>
       </View>
@@ -22,18 +24,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.revblue,
   },
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 200,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
   backgroundImage: {
-    ...StyleSheet.absoluteFillObject,
-    zIndex: -1,
+    width: 500,
+    height: 1000,
   },
   contentContainer: {
-    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 40,
+  },
+  logoContainer: {
+    marginBottom: 50, // Adjust the value to increase or decrease the space between the logo and other components
   },
   logoImage: {
-    width: 150,
-    height: 150,
+    width: 200,
+    height: 200,
   },
   title: {
     fontSize: 24,
