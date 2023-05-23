13848807d27c6889a9f7d3dc41df4b4b73f04f46
MihirW <mihirw@hotmail.com>
2023-05-23 11:19:32 +0530

    cleaned up code

--- components/common/Pages/WelcomePage/WelcomePage.js
+++ components/common/Pages/WelcomePage/WelcomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { COLORS, icons }  from '../../../constants';
+import { COLORS, icons }  from '../../../../constants';
 
 const WelcomePage = () => {
 
