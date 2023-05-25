40d2e7e3f1746947729f7ce3876df5914f9e1066
MihirW <mihirw@hotmail.com>
2023-05-25 21:44:23 +0530

    started with navigation, not working

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,11 +1,14 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import { WelcomePage } from '../components';
+import { WelcomePage, Dashboard } from '../components';
 
 //lets externalize this function
 const screens = {
     RevApp: {
         screen: WelcomePage
+    },
+    Dashboard: {
+        screen: Dashboard
     }
 }
 
