7e26e0fbb2ecbabe66c357db54204e5703a8356e
MWalve <mihirw@hotmail.com>
2023-06-30 07:01:10 -0400

    basic-fixes, still not working

--- components/common/Pages/Dashboard/Dashboard.js
+++ components/common/Pages/Dashboard/Dashboard.js
@@ -1,18 +1,19 @@
 //code
 import React from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { COLORS, icons }  from '../../../../constants';
 
 const Dashboard = () => {
-    //Health Quotes
-    <Text>
-        Hello
-    </Text>
-    //Favorites Widget
-    //Gut Widget
-    // Mental Health Widgets
-    //Taskbar
-}
+    return (
+        //Health Quotes
+        <Text>
+            Hello
+        </Text>
+        //Favorites Widget
+        //Gut Widget
+        // Mental Health Widgets
+        //Taskbar
+    )
+};
 //styles
 
 export default Dashboard;
