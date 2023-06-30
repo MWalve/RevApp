d1a91f206e6277596fc6f391fc54052a46d90e9b
MWalve <mihirw@hotmail.com>
2023-06-30 07:01:10 -0400

    basic-fixes, still not working

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,11 +1,15 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import { WelcomePage, Dashboard } from '../components';
+import WelcomePage from '../components/common/Pages/WelcomePage/WelcomePage';
+import Dashboard from '../components/common/Pages/Dashboard/Dashboard';
 
 //lets externalize this function
 const screens = {
     RevApp: {
-        screen: WelcomePage
+        screen: WelcomePage,
+        navigationOptions: {
+            headerShown: false,
+        },
     },
     Dashboard: {
         screen: Dashboard
