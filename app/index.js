f2a11579edb2b6cdb7d1d58471b418b27f66b905
MWalve <mihirw@hotmail.com>
2023-07-10 08:18:15 -0400

    idk-have to check

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 
 import { WelcomePage, Dashboard } from '../components';
 
