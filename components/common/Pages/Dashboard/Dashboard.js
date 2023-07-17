cb82ac1c821d2806a82efc3acc0ee8b5922806b2
MWalve <mihirw@hotmail.com>
2023-07-17 05:33:15 -0400

    added menu icon

--- components/common/Pages/Dashboard/Dashboard.js
+++ components/common/Pages/Dashboard/Dashboard.js
@@ -3,13 +3,19 @@ import React from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { COLORS, icons } from '../../../../constants';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Dashboard = () => {
     return (
+        <View style={styles.container}>
+            <Image source={icons.menu} style={styles.hamburger}/>
+            <Text style={styles.title}>
+                Welcome User!
+            </Text>
+        </View>
         //Health Quotes
-        <Text>
-            Hello
-        </Text>
+        
         //Favorites Widget
         //Gut Widget
         // Mental Health Widgets
@@ -17,5 +23,24 @@ const Dashboard = () => {
     )
 };
 //styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.gray,
+    },
+    title: {
+        fontSize: 24,
+        color: 'black',
+        marginBottom: 10,
+        paddingTop: 5,
+        paddingLeft: 10,
+    },
+    hamburger: {
+        position: 'absolute',
+        right: 10,
+        height: 40,
+        width: 30
+    }
+})
 
 export default Dashboard;
