f725fc9c72f6cf7aa94daf486734d78b629e55d1
MWalve <mihirw@hotmail.com>
2023-07-26 13:38:36 -0400

    added-quote

--- components/common/Pages/Dashboard/Dashboard.js
+++ components/common/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,52 @@
 //code
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 import { COLORS, icons } from '../../../../constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Dashboard = () => {
+
+    const [quote, setQuote] = useState('');
+
+    useEffect(() => {
+        //Function to generate a random quote index
+        const generateRandomQuoteIndex = (max) => Math.floor(Math.random() * max);
+
+        //sample list of quotes
+        const quotes = [
+            '"The road to health is paved with good intestines!"',
+            '"Sweetness is not in itself unhealthy, we simply eat only the most unhealthy kind of sweetness."',
+            '"Every day we live and every meal we eat we influence the great microbial organ inside us - for better or for worse.”',
+            '“. . . our body is an ecosystem. This ecosystem must be maintained . . .”',
+            '“Your gut is not Las Vegas. What happens in the gut does not stay in the gut.”',
+            '“The only bad workout is the one that didn’t happen.”',
+            '“Someone busier than you is running right now.”',
+            '“Working out is never convenient. But neither is illness, diabetes and obesity!”',
+            '"Exercise is king, nutrition is Queen, put them together, and youve got a kingdom."',
+        ]
+
+        //Get the current date
+        const currentDate = new Date().toDateString();
+
+        // Retrieve the stored date and quote index from AsyncStorage
+    AsyncStorage.getItem('lastQuoteDate').then((storedDate) => {
+        // If the stored date is different from the current date, generate a new random quote
+        if (storedDate !== currentDate) {
+          const randomQuoteIndex = generateRandomQuoteIndex(quotes.length);
+          setQuote(quotes[randomQuoteIndex]);
+  
+          // Store the new quote index and current date in AsyncStorage
+          AsyncStorage.setItem('lastQuoteIndex', randomQuoteIndex.toString());
+          AsyncStorage.setItem('lastQuoteDate', currentDate);
+        } else {
+          // If the stored date is the same as the current date, retrieve the last quote index and display the same quote
+          AsyncStorage.getItem('lastQuoteIndex').then((storedQuoteIndex) => {
+            setQuote(quotes[parseInt(storedQuoteIndex)]);
+          });
+        }
+      });
+    }, []);
+
     return (
         <View style={styles.container}>
             <Image source={icons.menu} style={styles.hamburger}/>
@@ -11,11 +54,9 @@ const Dashboard = () => {
                 Welcome User!
             </Text>
             <Text style={styles.quote}>
-                Random health quote for user
+                {quote}
             </Text>
         </View>
-        //Health Quotes
-        
         //Favorites Widget
         //Gut Widget
         // Mental Health Widgets
@@ -43,6 +84,7 @@ const styles = StyleSheet.create({
     },
     quote: {
         fontSize: 18,
+        fontStyle: 'italic',
         position: 'relative',
         fontFamily: 'san-serif',
         paddingLeft: 50,
