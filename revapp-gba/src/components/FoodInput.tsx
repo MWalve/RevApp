de0ab478fd8ba87726ed536768682555f86db84d
MWalve <mihirw@hotmail.com>
2024-11-10 22:15:57 -0500

    starting food input function

--- revapp-gba/src/components/FoodInput.tsx
+++ revapp-gba/src/components/FoodInput.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { StringLiteral } from 'typescript';
 
@@ -23,3 +23,43 @@ interface FoodLog {
     meal_type: 'breakfast' | 'lunch' | 'dinner' | 'snack';
     notes: string;
 }
+
+export default function FoodInput() {
+  const [categories, setCategories] = useState<FoodCategory[]>([]);
+  //const [foodItems, setFoodItems] = useState<FoodItem[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState(false);
+
+  const [foodLog, setFoodLog] = useState<FoodLog>({
+    food_item_id: '',
+    portion_size: '',
+    meal_type: 'breakfast',
+    notes: ''
+  });
+
+  const [newFoodItem, setNewFoodItem] = useState({
+    name: '',
+    category_id: '',
+  });
+
+  useEffect(() => {
+    fetchCategories();
+    //fetchFoodItems();
+  }, []);
+
+  async function fetchCategories() {
+    const { data, error } = await supabase
+        .from('food_categories')
+        .select('*')
+        .order('name', { ascending: true });
+
+    if (error) {
+      console.error('Error fetching cactegories:', error.message);
+      return;
+    }
+
+    setCategories(data || []);
+  }
+}
+
